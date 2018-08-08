524bddb43a6b463fcf775ed4ea15f3b233218fd2
myselfmonika11 <32818377+myselfmonika11@users.noreply.github.com>
2018-08-08 15:38:35 +0000

    Update Converting_Values.py

--- Converting_Values.py
+++ Converting_Values.py
@@ -1,14 +1,12 @@
 #Converting Values
 
 '''
-A=92
-print(type(A))
-print(type(str(A)))
+A=92                          
+print(type(A))                #Output: int
+print(type(str(A)))           #Output: str
 
 '''
 
-print(type(int("42")))
-print(type(float("2.5")))
-print(type(float("1")))
-
-
+print(type(int("42")))        #Output: int
+print(type(float("2.5")))     #Output: float
+print(type(float("1")))       #Output: float
