e0d035c88081a066422b177557a31084c45969a3
myselfmonika11 <32818377+myselfmonika11@users.noreply.github.com>
2018-08-08 15:54:48 +0000

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -2,42 +2,42 @@
 
 '''
 easy_as=[1,2,3]
-print(easy_as)
+print(easy_as)                          #Output: [1,2,3]
 
 
-empty=[]
-print(empty)
+empty=[]                                
+print(empty)                            #Output: []                
 letters=['a','b','c','d']
-print(letters)
+print(letters)                          #Output: [a,b,c,d]
 numbers=[2,3,5]
-print(numbers)
-mixed=[4,5,"seconds"]
-print(mixed)
+print(numbers)                          #Output: [2,3,5]
+mixed=[4,5,"seconds"]                   
+print(mixed)                            #Output: [4,5,'seconds']
 
 
 
 numbers=[2,3,5]
 numbers.append(7)
-print(numbers)
+print(numbers)                          #Output: [2,3,5,7]
 
-print(numbers[1])
+print(numbers[1])                       #Output: 3
 
 
 numbers=[2,3,5]
-print(numbers[0])
-print(numbers[-1])
-print(numbers[1:-1])
+print(numbers[0])                       #Output: 2
+print(numbers[-1])                      #Output: 7
+print(numbers[1:-1])                    #Output: [3,5]
 letters=['a','b','c','d']
-print(letters[:3])
+print(letters[:3])                      #Output: ['a','b','c']
 
 '''
 
 #Nested list
 
-Number=[1,2,3,4,5,6]
+Number=[1,2,3,4,5,6]                    
 Alphabet=['A','B','C','D','E']
 Nested_List=[Number,Alphabet]
-print(Nested_List)
-print(Nested_List[0])
-print(Nested_List[0][1])
+print(Nested_List)                      #Output: [[1,2,3,4,5,6],['A','B','C','D','E']]
+print(Nested_List[0])                   #Output: [1,2,3,4,5,6]
+print(Nested_List[0][1])                #Output: 2
 
