b459294d6aab2a5f00e2d905411001fef16f4709
myselfmonika11 <32818377+myselfmonika11@users.noreply.github.com>
2018-08-24 14:20:50 +0530

    Add files via upload

---
+++ PythonCA_1.py
@@ -0,0 +1,8 @@
+#Q.1
+#Assume an employee of an organisation, using python data structure.
+#Store the details of employee and display using looping techniques.
+
+Employee_Details=[("Name: Monika Singh", "Employee_id: 11700019"), {"Designation":'Product_Manager', "Contact":9876543210, "Salary":30000}]
+
+for i in range(1):
+    print(Employee_Details)
